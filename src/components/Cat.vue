f9229bc5b41bd5553658994f6b5d02b4bcb827ef
nikolay_koskarev <rokealvo@gmail.com>
2024-05-02 16:11:29 +0700

    sf

--- src/components/Cat.vue
+++ src/components/Cat.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Todo from './Todo.vue'
 import {ref} from 'vue'
+import {Category} from "../todoStore";
 
 const props = defineProps<{ cat: Category }>();
+
 const input = ref('')
+
 function addTodo() {
   props.cat.addTodo(input.value)
 }
@@ -11,14 +14,17 @@ function addTodo() {
 
 <template>
   <li>
-    <h2>{{ props.cat.name }}</h2>
-    <p>len: {{ props.cat.len }}</p>
+    <h2>{{ props.cat.name }}
+      <span style="font-size: 0.5em">
+        (len: {{ props.cat.len }})
+      </span>
+    </h2>
     <h3>todos: </h3>
   <ul>
-    <todo 
+    <todo
       v-for="todo in props.cat.todos"
       :todo="todo"
-    /> 
+    />
   </ul>
   <div>
     <span>add todo: </span>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import type {ToDo} from '../todoStore'
+
 const props = defineProps<{ todo: ToDo }>();
 </script>
 
 <template>
   <li>
-    <p>todo</p>
+    {{props.todo.value}}
   </li>
 </template>
